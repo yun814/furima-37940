78f76e4102970e6acc143695b77aae05c12372a4
yun814 <magisuteru.magi@gmail.com>
2022-05-19 17:33:33 +0900

    prefectureのActiveHash作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   validates :explanation, presence: true
   validates :category_id, numericality: {other_than: 1}
   validates :condition_id, numericality: {other_than: 1}
-  validates :delivery_charge_id, {other_than: 1}
-  validates :prefecture_id, presence: true
+  validates :delivery_charge_id, numericality: {other_than: 1}
+  validates :prefecture_id, numericality: {other_than: 1}
   validates :shipping_day_id, presence: true
   validates :price, presence: true
 
@@ -14,4 +14,5 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_charge
+  belongs_to :prefecture
 end
