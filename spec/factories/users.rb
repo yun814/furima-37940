86911f1cd48b861d87792b57adf7dd66fe93de7d
yun814 <magisuteru.magi@gmail.com>
2022-05-19 14:21:11 +0900

    ユーザーのFactoryBotを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Gimei.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'1a' + Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
     last_name_kanji       {nickname.last}
     first_name_kanji      {nickname.first}
