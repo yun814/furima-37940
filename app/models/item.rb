35a32a5fd4287a169d6e8a4cf1093569e81c456b
yun814 <magisuteru.magi@gmail.com>
2022-05-19 18:45:04 +0900

    エラーメッセージの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, numericality: {other_than: 1}
-  validates :condition_id, numericality: {other_than: 1}
-  validates :delivery_charge_id, numericality: {other_than: 1}
-  validates :prefecture_id, numericality: {other_than: 1}
-  validates :shipping_day_id, numericality: {other_than: 1}
-  validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A\d+\z/ }
-  validates :image, presence: true
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}, format: { with: /\A\d+\z/, message: "is invalid. Input half-width characters" }
+  validates :category_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :delivery_charge_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :shipping_day_id, numericality: {other_than: 1, message: "can't be blank"}
 
   has_one_attached :image
   belongs_to :user
