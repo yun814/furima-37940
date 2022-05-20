d83d8b774ab73a64768e5f5144f3da4b1d3b82c0
yun814 <magisuteru.magi@gmail.com>
2022-05-20 19:01:26 +0900

    itemモデルの単体テスト実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name               {Faker::Commerce.product_name}
+    explanation        {Faker::Lorem.paragraph}
+    price              {Faker::Number.within(range: 300..9999999)}
+    category           {Category.all.sample}
+    condition          {Condition.all.sample}
+    delivery_charge    {DeliveryCharge.all.sample}
+    prefecture         {Prefecture.all.sample}
+    shipping_day       {ShippingDay.all.sample}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
