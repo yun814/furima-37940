f76401bb0cc62394bc607ae20aa037b2c8ea1090
yun814 <magisuteru.magi@gmail.com>
2022-05-21 21:30:29 +0900

    他者の商品削除を制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.order("id DESC")
