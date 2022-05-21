72d25e4ec4e5ef6aab70aaccb6a30314c103a810
yun814 <magisuteru.magi@gmail.com>
2022-05-21 19:06:16 +0900

    edit,updateアクションをルーティングとコントローラーに追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.order("id DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -22,8 +22,24 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
