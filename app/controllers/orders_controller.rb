3c8c9a76bd712e8a763d1b39382073f6338cfc56
yun814 <magisuteru.magi@gmail.com>
2022-05-23 22:34:55 +0900

    購入情報の保存機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+
   def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :telephone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,17 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :municipality, :address, :building, :telephone_number
+
+  validates :item_id, presence: true
+  validates :user_id, presence: true
+  validates :postal_code, presence: true, format: {with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :municipality, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, telephone_number: telephone_number, order_id: order.id)
+  end
+end
