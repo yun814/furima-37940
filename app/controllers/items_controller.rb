a5e73b6ebbae57c1bff06237e52796b5ed52cb4f
yun814 <magisuteru.magi@gmail.com>
2022-05-23 14:43:03 +0900

    商品削除機能のエラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
 
   def index
