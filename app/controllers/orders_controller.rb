c459268decc1ddafa6842c4c18106b803e57102f
yun814 <magisuteru.magi@gmail.com>
2022-05-24 17:27:10 +0900

    自身の商品の購入ページへの遷移を制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :set_item
+  before_action :move_to_index
 
   def index
     @order_address = OrderAddress.new
@@ -23,6 +24,12 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def pay_item
