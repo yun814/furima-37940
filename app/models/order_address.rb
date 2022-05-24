b3a78225825008926fd394dfccde8298f3162b0b
yun814 <magisuteru.magi@gmail.com>
2022-05-24 17:18:14 +0900

    order_addressクラスのバリデーションを追加・修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,12 +4,13 @@ class OrderAddress
 
   validates :item_id, presence: true
   validates :user_id, presence: true
+  validates :token, presence: true
   validates :postal_code, presence: true, format: {with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   validates :municipality, presence: true
   validates :address, presence: true
   validates :telephone_number, presence: true, format: {with: /\A\d+\z/, message: "is invalid. Input only number"}
-  validates :telephone_number, format: {with: /\A*{10,11}\z/, message: "is too short"}
+  validates :telephone_number, format: {with: /\A.{10,11}\z/, message: "is too short"}
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
